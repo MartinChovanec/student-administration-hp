4d910b5b9408beba164fe4e3f3c9cbe13edf5b9d
Martin Chovanec <chovanec.m11@gmail.com>
2024-04-05 00:48:48 +0200

    studentList added

---
+++ src/components/StudentList.jsx
@@ -0,0 +1,13 @@
+
+export const StudentList = () => {
+    return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> změna.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
