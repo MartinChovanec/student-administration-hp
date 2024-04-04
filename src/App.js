e5d18d34ff24c9a540a14be97d5551320274da87
Martin Chovanec <chovanec.m11@gmail.com>
2024-04-05 00:26:14 +0200

    text edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to relooad.
+          Edit <code>src/App.js</code> and save to rd.
         </p>
         <a
           className="App-link"
