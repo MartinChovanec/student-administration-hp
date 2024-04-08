e9d37634b5eb3067cefbc7f34fa759221c20ba79
Martin Chovanec <chovanec.m11@gmail.com>
2024-04-08 23:17:57 +0200

    components added to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { StudentList } from "./components/StudentList";
+import { StudentCreateForm } from "./components/StudentCreateForm";
+import { StudentDetail } from "./components/StudentDetail";
+import { StudentEditForm } from "./components/StudentEditForm";
 
 
 const router = createBrowserRouter([
   {
     path: "/student-administration-hp",
     element: <StudentList />,
-  }
+  },
+  {
+    path: "/students/:id",
+    element: <StudentDetail />,
+  },
+  {
+    path: "/students/:id/edit",
+    element: <StudentEditForm />,
+  },
+  {
+    path: "/students/create",
+    element: <StudentCreateForm />,
+  },
 ]);
 
 function App() {
